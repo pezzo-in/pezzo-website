a0555ea1d00aba61f49b9b5a6c6883a2818fdadc
pavan <pavan@graphicana.co.in>
2014-05-09 15:22:36 +0530

    Corrected socialfeed jquery bug, now need to style this to show social media properly

--- social-feed.php
+++ social-feed.php
@@ -1,48 +1,22 @@
-
     <!-- Social-feed css -->
-    <link href="css/jquery.socialfeed.css" rel="stylesheet" type="text/css">
+    <link href="stylesheets/jquery.socialfeed.css" rel="stylesheet" type="text/css">
     <!-- font-awesome for social network icons -->
     <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
-    <!--<style>
-    /*This style is needed only for the plugin demo page. Do not use it in your projects*/
-    .credits{
-        text-align:center;padding:20px;margin-top:10px;font-size:12px;
-    }
-    body{ 
-        font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-        font-size: 14px;
-        line-height: 20px;
-        color: #333333;
-        background-color: #ffffff;
-        text-align:center;
-    }
-    a{color: #0088cc;text-decoration: none;}
-    a:hover,a:focus {color: #005580;text-decoration: underline;}
-    .muted {color: #999999;}
-    .container {display:inline-block;width:600px;}
-    </style>-->
-
 
     <div class="container">
-        <h3 class="muted">social - feed</h3>
-        <iframe src="http://ghbtns.com/github-btn.html?user=pavelk2&amp;repo=social-feed&amp;type=watch&amp;count=true&amp;size=large" allowtransparency="true" frameborder="0" scrolling="0" width="152px" height="30px"></iframe>
-        <iframe src="http://ghbtns.com/github-btn.html?user=pavelk2&amp;repo=social-feed&amp;type=fork&amp;count=true&amp;size=large" allowtransparency="true" frameborder="0" scrolling="0" width="152px" height="30px"></iframe>
-        
-        <h4 class="muted">Tesla Motors</h4>
         <div class="social-feed-container">
         </div>
-        <h6 class="credits muted"><a href="https://github.com/pavelk2/social-feed" target="_blank">Social-feed</a> jquery plugin, developed by <a target="_blank" href="http://kucherbaev.com">Pavel Kucherbaev</a></h6>
     </div>
 
     <!-- jQuery -->
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
 
     <!-- doT.js for rendering templates and moment.js for showing time ago -->
-    <script src="dependencies/doT.min.js"></script>
-    <script src="dependencies/moment.min.js"></script>
+    <script src="javascripts/doT.min.js"></script>
+    <script src="javascripts/moment.min.js"></script>
 
     <!-- Social-feed js -->
-    <script src="js/jquery.socialfeed.js"></script>
+    <script src="javascripts/jquery.socialfeed.js"></script>
 
     <script>
     $(document).ready(function(){
@@ -55,7 +29,7 @@
                     },
                     //VK--------------------
                     vk:{
-                        accounts:[125936523], //id for users and -id (with minus) for groups 
+                        accounts:[125936523], //id for users and -id (with minus) for groups
                         limit:2,
                         source:'all'
                     },
